71a1573f75d1397111345b7fe0b88f33a839cb11
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-04-16 22:35:52 +0300

    Initial commit Create player movement Added player tracking for main camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeedToForward;
+    [SerializeField] float moveSpeedToSide;
+    [SerializeField] float rotationSpeed;
+
+    void Update()
+    {
+        TouchPosition touchPosition = TouchHandler.GetTouchPosition();
+        if(touchPosition == TouchPosition.Right)
+        {
+            transform.Translate(moveSpeedToSide * Vector3.right, Space.World);
+            transform.Rotate(0f, rotationSpeed, 0f);
+        }
+        else if (touchPosition == TouchPosition.Left)
+        {
+           transform.Translate(moveSpeedToSide * Vector3.left, Space.World);
+           transform.Rotate(0f, -rotationSpeed, 0f);
+        }
+        transform.Translate(moveSpeedToForward * Vector3.forward, Space.World);
+    }
+}

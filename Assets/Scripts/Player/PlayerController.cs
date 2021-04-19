36ba47e0e90a980dfd284fa4488acd93a1f45c1a
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-04-19 19:52:55 +0300

    Add a player burst when he collides with an obstacle

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    [SerializeField] GameObject playerBurst;
+
+    PlayerMovementController movementController;
+    MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        movementController = GetComponent<PlayerMovementController>();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Obstacle"))
+        {
+            playerBurst.SetActive(true);
+            meshRenderer.enabled = movementController.enabled = false;
+            //Game over
+        }
+    }
+}

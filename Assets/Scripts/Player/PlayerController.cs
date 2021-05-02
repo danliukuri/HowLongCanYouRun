5cd7eadf72e3eb7a660fd82316531ad7c4ca5497
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-05-02 14:59:00 +0300

    Add destruction boundary of objects Remove burst prefab from player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,26 +1,32 @@
 ﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    
+{   
+    #region Fields
     [SerializeField] GameObject playerBurst;
 
     PlayerMovementController movementController;
     MeshRenderer meshRenderer;
+    Material material;
+    #endregion
 
+    #region Methods
     void Awake()
     {
         movementController = GetComponent<PlayerMovementController>();
         meshRenderer = GetComponent<MeshRenderer>();
+        material = GetComponent<Renderer>().material;
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.CompareTag("Obstacle"))
         {
-            playerBurst.SetActive(true);
+            GameObject gameObject = Instantiate(playerBurst, transform.position, transform.rotation, transform);
+            gameObject.GetComponent<Renderer>().material = material;
             meshRenderer.enabled = movementController.enabled = false;
             //Game over
         }
     }
+    #endregion
 }
